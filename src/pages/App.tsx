fbd56db54c8aa9168ed469e20156425a58eca4fe
AndreRichartz <andrerichartz07@gmail.com>
2025-06-11 10:12:45 -0300

    feat: developed App.tsx #5

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
-
+import './styles/App.css'   
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import  Home  from './pages/Home.tsx'
+import  NoticeBoard from './pages/Notice-board.tsx'
+import Inspection from './pages/Inspection.tsx'
+import ReportandAnalisys from './pages/Report-and-Analisys.tsx'
+import RouteManagement from './pages/Route-management.tsx'
 function App() {
-  return (
-    <div>App</div>
-  )
+ 
+    
+    return (
+
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/notice-board" element={<NoticeBoard/>}/>
+                <Route path="/inspection" element={<Inspection/>}/>
+                <Route path="/report-and-analisys" element={<ReportandAnalisys/>}/>
+                <Route path="/route-management" element={<RouteManagement/>}/>
+            </Routes>
+        </Router>
+
+    )
 }
 
 export default App
