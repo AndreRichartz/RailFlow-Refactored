e9805fa3297691c235032bb5e20dc66ecccdede0
KruegerL <laura_krueger@estudante.sesisenai.org.br>
2025-06-13 11:35:55 -0300

    feat: tela de graficos feitas #17

--- src/pages/ReportAndAnalisys.tsx
+++ src/pages/ReportAndAnalisys.tsx
@@ -1,9 +1,129 @@
-import React from 'react'
-
+import React, {useRef} from "react";
+import Header from "../components/Header";
+import Titlenoticeboard from "../components/TitleNoticeBoard";
+import styles from "../styles/ReportAndAnalisys.module.css";
+import Horizontalselection from "../components/HorizontalSelection";
+import Horizontalselectioncard from "../components/HorizontalSelectionCard";
+import { useState } from "react";
 function ReportAndAnalisys() {
+  const [selectedCard, setSelectedCard] = useState<string | null>(null);
+
+  const handleCardClick = (cardContent: string) => {
+    setSelectedCard(cardContent === selectedCard ? null : cardContent);
+    console.log(selectedCard)
+  };
+
+  
+  const scrollRef = useRef<HTMLDivElement>(null);
+      const isDown = useRef(false);
+      const startX = useRef(0);
+      const scrollLeft = useRef(0);
+  
+      const handleMouseDown = (e: React.MouseEvent) => {
+          isDown.current = true;
+          startX.current = e.pageX - (scrollRef.current?.offsetLeft || 0);
+          scrollLeft.current = scrollRef.current?.scrollLeft || 0;
+          document.body.style.cursor = "grabbing";
+      };
+  
+      const handleMouseLeave = () => {
+          isDown.current = false;
+          document.body.style.cursor = "";
+      };
+  
+      const handleMouseUp = () => {
+          isDown.current = false;
+          document.body.style.cursor = "";
+      };
+  
+      const handleMouseMove = (e: React.MouseEvent) => {
+          if (!isDown.current) return;
+          e.preventDefault();
+          const x = e.pageX - (scrollRef.current?.offsetLeft || 0);
+          const walk = x - startX.current;
+          if (scrollRef.current) {
+              scrollRef.current.scrollLeft = scrollLeft.current - walk;
+          }
+      };
+
+  const cardContents = [
+    "8795",
+    "8865",
+    "8465",
+    "8875",
+    "8455",
+    "8235",
+    "8765",
+    "8725",
+    "8855",
+    "8655",
+    "2345",
+    "4153",
+    "9578",
+    "3457",
+    "0786",
+    "1345",
+    "8780",
+    "3379",
+    "1234",
+    "4567",
+    "7890",
+    "2346",
+    "5678",
+  ];
+
   return (
-    <div>ReportAndAnalisys</div>
-  )
+    <>
+      <Header />
+      <div className={styles["report-wrapper"]}>
+        <Titlenoticeboard title={"Relatório Geral"} />
+        <div className={styles['graph-1-wrapper']}>
+        <img
+          src="src\assets\img\financial-graphs-and-charts-in-excel.png"
+          alt=""
+          className={styles["graph-1"]}
+        />
+        </div>
+        <Titlenoticeboard title={"Análise dos Trens"} />
+       <div className={styles["horizontal-selection-wrapper"]}>
+       <Horizontalselection>
+        {cardContents.map((content) => (
+          <Horizontalselectioncard
+            key={content}
+            child={content}
+            isSelected={selectedCard === content}
+            onClick={() => handleCardClick(content)}
+          />
+        ))}
+      </Horizontalselection>
+      
+      </div>
+      <span className={styles['train']}><p>Trem:</p>{selectedCard}</span>
+      <span className={styles['route']}><p>Rota do momento:</p>1567</span>
+      {selectedCard !== null &&
+      <div className={styles['graphs-wrapper']}
+      >
+<img
+          src="src\assets\img\financial-graphs-and-charts-in-excel.png"
+          alt=""
+          className={styles["graphs"]}
+          
+        />
+        <img
+          src="src\assets\img\financial-graphs-and-charts-in-excel.png"
+          alt=""
+          className={styles["graphs"]}
+        />
+        <img
+          src="src\assets\img\financial-graphs-and-charts-in-excel.png"
+          alt=""
+          className={styles["graphs"]}
+        />
+      </div>
+}
+      </div>
+    </>
+  );
 }
 
-export default ReportAndAnalisys
+export default ReportAndAnalisys;
